7e73fc2e012e01f32eb89802b2915e181404c8d6
Silviu Dinca <silviudinca412@gmail.com>
2023-08-08 19:16:40 +0300

    new endpoints added -get contacts -add contact

--- main.ts
+++ main.ts
@@ -1,12 +1,28 @@
 import {Express, Request, Response } from 'express'
 import express from 'express'
 import { ContactController } from './ContactController'
+import { Contact } from './Contact'
+import bodyParser, { BodyParser } from 'body-parser'
 
 const app: Express = express()
 const contactController = new ContactController()
 
+const jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.get('/', (req: Request, res: Response) => {
-  res.json()
+  contactController.getContacts().then(  contacts => res.json(contacts) ).catch(err => { 
+    console.log(err)
+    res.status(501)
+  })
+})
+
+app.post('/addContact', jsonParser, (req: Request, res: Response) => {
+  const newContact = req.body as Contact
+  contactController.addContact(newContact).then( newContact => res.json(newContact) ).catch( err => {
+    console.log(err)
+    res.status(501)
+  })
 })
 
 app.listen(5000, () => {
