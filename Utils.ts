f32363d3328ecf1d19ea6b1caa3bb5f4efa98a65
Silviu Dinca <silviudinca412@gmail.com>
2023-08-08 19:44:01 +0300

    added error middleware replaced .then .catch with async await routes stored in a global class with constants

---
+++ Utils.ts
@@ -0,0 +1,5 @@
+export default class RoutesConstants {
+    public static getContacts = '/contacts'
+    public static addContact = '/addContact'
+    public static getContactById = '/contacts/:id'
+}
